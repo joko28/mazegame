d88adf3ef244e3dabe060519c004f124d56dd211
jnjoko <56281870+joko28@users.noreply.github.com>
2022-04-17 17:54:32 -0700

    update files

--- sketch.js
+++ sketch.js
@@ -5,70 +5,73 @@
 //left and right keys to move the sprite
 //it's position is adjusted to another sprite's opaque pixels
 
-var triangle;
+var monster;
 var cloud;
 var platform;
-var myFont;
+//var myFont;
 
 var GRAVITY = 1;
 
 function preload() {
-  myFont = loadFont('assets/fonts/Monoton.Regular.ttf');
+  myFont = loadFont('assets/fonts/Monoton-Regular.ttf');
 }
 
 function setup() {
-//  createCanvas(800, 400);
+  //createCanvas(800, 400);
   var cnv = createCanvas(windowWidth, windowHeight);
     cnv.style('display', 'block');
-  triangle = createSprite(300, 150);
-  triangle.addAnimation('normal', 'assets/triangle.png');
-  triangle.debug = true;
+    background(229, 204, 255);
 
-  cloud = createSprite(500, 150);
-  cloud.addAnimation('normal', 'assets/cloud_breathing0001.png', 'assets/cloud_breathing0009.png');
-  cloud.addAnimation('transformed', 'assets/asterisk_normal0001.png', 'assets/asterisk_normal0003.png');
-  cloud.setCollider('circle', 0, 0, 50);
-  cloud.debug = true;
+  monster = createSprite(300, 100);
+  monster.addAnimation('normal', 'assets/monster.png');
+  monster.debug = true;
 
-  platform = createSprite(400, 300);
+  sun = createSprite(500, 100);
+  sun.addAnimation('normal', 'assets/sun1.png', 'assets/sun3.png');
+  //sun.addAnimation('transformed', 'assets/monster.png');
+  //sun.setCollider('circle', 0, 0, 50);
+  sun.debug = true;
+
+  platform = createSprite(400, 250);
   platform.addImage(loadImage('assets/platform.png'));
 
-  triangle.depth = 10;
+  monster.depth = 8;
 }
 
 function draw() {
-  background(255, 255, 255);
+  //background(255, 255, 255);
 
   //if no arrow input set velocity to 0
-  triangle.velocity.x = 0;
+  monster.velocity.x = 0;
 
   if (keyIsDown(LEFT_ARROW))
-    triangle.velocity.x = -5;
+    monster.velocity.x = -5;
   if (keyIsDown(RIGHT_ARROW))
-    triangle.velocity.x = 5;
+    monster.velocity.x = 5;
 
   //instead of checking the colliders or bounding box overlaps
   //I can just check a point against a collider
-  if(cloud.overlapPoint(triangle.position.x, triangle.position.y))
-    cloud.changeAnimation('transformed');
+  //if(sun.overlapPoint(triangle.position.x, triangle.position.y))
+  //  sun.changeAnimation('transformed');
 
   //Or check a point against the pixels of a sprite animation or image
   //if the bottom of the triangle is not overlapping with the non transparent pixels
   //of the platform make it fall
-  if(platform.overlapPixel(triangle.position.x, triangle.position.y+30)==false)
-    triangle.velocity.y += GRAVITY;
+  if(platform.overlapPixel(monster.position.x, monster.position.y+30)==false)
+    monster.velocity.y += GRAVITY;
 
   //if the bottom of the triangle is overlapping the non transparent pixels
   //of the platform move it up one pixel until it doesn't overlap anymore
-  while(platform.overlapPixel(triangle.position.x, triangle.position.y+30))
+  while(platform.overlapPixel(monster.position.x, monster.position.y+30))
   {
-    triangle.position.y--;
-    triangle.velocity.y = 0;
+    monster.position.y--;
+    monster.velocity.y = 0;
   }
 
   drawSprites();
-  drawText(font);
-  windowResized();
+  //drawBlock();
+  //drawText(font);
+  //windowResized();
 }
 
 function drawText(font) {
